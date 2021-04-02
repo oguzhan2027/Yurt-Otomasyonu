06f084ed00cf6be2987de9ceabb5a3ca4f8c0ed2
oguzhan celik <oguzhancelik2027@gmail.com>
2021-04-02 14:24:11 +0300

    1.commit

---
+++ YurtKayıtSistemi/Form1.Designer.cs
@@ -0,0 +1,341 @@
+﻿
+namespace YurtKayıtSistemi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtOgrAd = new System.Windows.Forms.TextBox();
+            this.txtOgrSoyad = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.mskTC = new System.Windows.Forms.MaskedTextBox();
+            this.mskOgrTelefon = new System.Windows.Forms.MaskedTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.mskDogum = new System.Windows.Forms.MaskedTextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.cmbBolum = new System.Windows.Forms.ComboBox();
+            this.txtMail = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.cmbOdaNo = new System.Windows.Forms.ComboBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtVeliAdSoyad = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.mskVeliTelefon = new System.Windows.Forms.MaskedTextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.rchAdres = new System.Windows.Forms.RichTextBox();
+            this.btnKaydet = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(43, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Öğrenci Adı : ";
+            // 
+            // txtOgrAd
+            // 
+            this.txtOgrAd.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtOgrAd.Location = new System.Drawing.Point(182, 14);
+            this.txtOgrAd.Name = "txtOgrAd";
+            this.txtOgrAd.Size = new System.Drawing.Size(203, 29);
+            this.txtOgrAd.TabIndex = 1;
+            // 
+            // txtOgrSoyad
+            // 
+            this.txtOgrSoyad.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtOgrSoyad.Location = new System.Drawing.Point(182, 60);
+            this.txtOgrSoyad.Name = "txtOgrSoyad";
+            this.txtOgrSoyad.Size = new System.Drawing.Size(203, 29);
+            this.txtOgrSoyad.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(43, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(136, 21);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Öğrenci SoyAdı : ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(43, 117);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 21);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "TC No :";
+            // 
+            // mskTC
+            // 
+            this.mskTC.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.mskTC.Location = new System.Drawing.Point(182, 109);
+            this.mskTC.Mask = "00000000000";
+            this.mskTC.Name = "mskTC";
+            this.mskTC.Size = new System.Drawing.Size(203, 29);
+            this.mskTC.TabIndex = 6;
+            this.mskTC.ValidatingType = typeof(int);
+            // 
+            // mskOgrTelefon
+            // 
+            this.mskOgrTelefon.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.mskOgrTelefon.Location = new System.Drawing.Point(182, 159);
+            this.mskOgrTelefon.Mask = "(999) 000-0000";
+            this.mskOgrTelefon.Name = "mskOgrTelefon";
+            this.mskOgrTelefon.Size = new System.Drawing.Size(203, 29);
+            this.mskOgrTelefon.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(43, 167);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 21);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Telefon :";
+            // 
+            // mskDogum
+            // 
+            this.mskDogum.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.mskDogum.Location = new System.Drawing.Point(182, 208);
+            this.mskDogum.Mask = "00/00/0000";
+            this.mskDogum.Name = "mskDogum";
+            this.mskDogum.Size = new System.Drawing.Size(203, 29);
+            this.mskDogum.TabIndex = 10;
+            this.mskDogum.ValidatingType = typeof(System.DateTime);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(43, 216);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(114, 21);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Doğum Tarihi :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(43, 260);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(140, 21);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Öğrenci Bölümü : ";
+            // 
+            // cmbBolum
+            // 
+            this.cmbBolum.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.cmbBolum.FormattingEnabled = true;
+            this.cmbBolum.Location = new System.Drawing.Point(182, 260);
+            this.cmbBolum.Name = "cmbBolum";
+            this.cmbBolum.Size = new System.Drawing.Size(203, 29);
+            this.cmbBolum.TabIndex = 12;
+            // 
+            // txtMail
+            // 
+            this.txtMail.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtMail.Location = new System.Drawing.Point(182, 314);
+            this.txtMail.Name = "txtMail";
+            this.txtMail.Size = new System.Drawing.Size(203, 29);
+            this.txtMail.TabIndex = 14;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(43, 322);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(53, 21);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Mail : ";
+            // 
+            // cmbOdaNo
+            // 
+            this.cmbOdaNo.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.cmbOdaNo.FormattingEnabled = true;
+            this.cmbOdaNo.Location = new System.Drawing.Point(182, 363);
+            this.cmbOdaNo.Name = "cmbOdaNo";
+            this.cmbOdaNo.Size = new System.Drawing.Size(203, 29);
+            this.cmbOdaNo.TabIndex = 16;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label8.Location = new System.Drawing.Point(39, 363);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(78, 21);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Oda No : ";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // txtVeliAdSoyad
+            // 
+            this.txtVeliAdSoyad.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtVeliAdSoyad.Location = new System.Drawing.Point(182, 406);
+            this.txtVeliAdSoyad.Name = "txtVeliAdSoyad";
+            this.txtVeliAdSoyad.Size = new System.Drawing.Size(203, 29);
+            this.txtVeliAdSoyad.TabIndex = 18;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label9.Location = new System.Drawing.Point(39, 414);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(122, 21);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "Veli Ad Soyad : ";
+            // 
+            // mskVeliTelefon
+            // 
+            this.mskVeliTelefon.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.mskVeliTelefon.Location = new System.Drawing.Point(182, 456);
+            this.mskVeliTelefon.Mask = "(999) 000-0000";
+            this.mskVeliTelefon.Name = "mskVeliTelefon";
+            this.mskVeliTelefon.Size = new System.Drawing.Size(203, 29);
+            this.mskVeliTelefon.TabIndex = 20;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label10.Location = new System.Drawing.Point(39, 464);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(102, 21);
+            this.label10.TabIndex = 19;
+            this.label10.Text = "Veli Telefon :";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label11.Location = new System.Drawing.Point(39, 509);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(91, 21);
+            this.label11.TabIndex = 21;
+            this.label11.Text = "Veli Adres :";
+            // 
+            // rchAdres
+            // 
+            this.rchAdres.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.rchAdres.Location = new System.Drawing.Point(182, 512);
+            this.rchAdres.Name = "rchAdres";
+            this.rchAdres.Size = new System.Drawing.Size(203, 96);
+            this.rchAdres.TabIndex = 22;
+            this.rchAdres.Text = "";
+            // 
+            // btnKaydet
+            // 
+            this.btnKaydet.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnKaydet.Location = new System.Drawing.Point(182, 629);
+            this.btnKaydet.Name = "btnKaydet";
+            this.btnKaydet.Size = new System.Drawing.Size(203, 43);
+            this.btnKaydet.TabIndex = 23;
+            this.btnKaydet.Text = "Kaydet";
+            this.btnKaydet.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(185)))), ((int)(((byte)(248)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(463, 692);
+            this.Controls.Add(this.btnKaydet);
+            this.Controls.Add(this.rchAdres);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.mskVeliTelefon);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.txtVeliAdSoyad);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.cmbOdaNo);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txtMail);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.cmbBolum);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.mskDogum);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.mskOgrTelefon);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.mskTC);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtOgrSoyad);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtOgrAd);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Öğrenci Kayıt";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtOgrAd;
+        private System.Windows.Forms.TextBox txtOgrSoyad;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.MaskedTextBox mskTC;
+        private System.Windows.Forms.MaskedTextBox mskOgrTelefon;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.MaskedTextBox mskDogum;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox cmbBolum;
+        private System.Windows.Forms.TextBox txtMail;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox cmbOdaNo;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtVeliAdSoyad;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.MaskedTextBox mskVeliTelefon;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.RichTextBox rchAdres;
+        private System.Windows.Forms.Button btnKaydet;
+    }
+}
+
