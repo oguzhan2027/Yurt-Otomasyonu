cae426eaa86c7822842c7e457143ee921c6a38b6
oguzhan celik <oguzhancelik2027@gmail.com>
2021-04-16 01:09:11 +0300

    commit

---
+++ YurtKayıtSistemi/FrmOgrListe.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace YurtKayıtSistemi
+{
+    public partial class FrmOgrListe : Form
+    {
+        public FrmOgrListe()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmOgrListe_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'yurtOtomasyonuDataSet3.Ogrenci' table. You can move, or remove it, as needed.
+            this.ogrenciTableAdapter.Fill(this.yurtOtomasyonuDataSet3.Ogrenci);
+
+        }
+    }
+}
