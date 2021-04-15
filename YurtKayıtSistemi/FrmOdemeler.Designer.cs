886e78dc3afe825c6b66391e2a0ad13282a9a09a
oguzhan celik <oguzhancelik2027@gmail.com>
2021-04-16 01:04:22 +0300

    commit

--- YurtKayıtSistemi/FrmOdemeler.Designer.cs
+++ YurtKayıtSistemi/FrmOdemeler.Designer.cs
@@ -33,7 +33,7 @@ namespace YurtKayıtSistemi
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmOdemeler));
             this.label1 = new System.Windows.Forms.Label();
             this.txtOgrid = new System.Windows.Forms.TextBox();
-            this.txtOdenen = new System.Windows.Forms.TextBox();
+            this.txtOgrenciad = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.txtKalan = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
@@ -46,6 +46,10 @@ namespace YurtKayıtSistemi
             this.ogrAdDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.ogrSoyadDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.ogrKalanBorcDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.txtOgrencisoyad = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtOdenen = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.yurtOtomasyonuDataSet2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.borclarBindingSource)).BeginInit();
@@ -55,7 +59,7 @@ namespace YurtKayıtSistemi
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label1.Location = new System.Drawing.Point(59, 226);
+            this.label1.Location = new System.Drawing.Point(57, 214);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(98, 21);
             this.label1.TabIndex = 0;
@@ -64,33 +68,33 @@ namespace YurtKayıtSistemi
             // txtOgrid
             // 
             this.txtOgrid.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.txtOgrid.Location = new System.Drawing.Point(186, 218);
+            this.txtOgrid.Location = new System.Drawing.Point(184, 214);
             this.txtOgrid.Name = "txtOgrid";
             this.txtOgrid.Size = new System.Drawing.Size(187, 29);
             this.txtOgrid.TabIndex = 1;
             // 
-            // txtOdenen
+            // txtOgrenciad
             // 
-            this.txtOdenen.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.txtOdenen.Location = new System.Drawing.Point(186, 270);
-            this.txtOdenen.Name = "txtOdenen";
-            this.txtOdenen.Size = new System.Drawing.Size(187, 29);
-            this.txtOdenen.TabIndex = 3;
+            this.txtOgrenciad.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtOgrenciad.Location = new System.Drawing.Point(184, 266);
+            this.txtOgrenciad.Name = "txtOgrenciad";
+            this.txtOgrenciad.Size = new System.Drawing.Size(187, 29);
+            this.txtOgrenciad.TabIndex = 3;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label2.Location = new System.Drawing.Point(59, 278);
+            this.label2.Location = new System.Drawing.Point(57, 266);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(68, 21);
+            this.label2.Size = new System.Drawing.Size(101, 21);
             this.label2.TabIndex = 2;
-            this.label2.Text = "Ödenen";
+            this.label2.Text = "Öğreni Adı : ";
             // 
             // txtKalan
             // 
             this.txtKalan.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.txtKalan.Location = new System.Drawing.Point(186, 331);
+            this.txtKalan.Location = new System.Drawing.Point(184, 408);
             this.txtKalan.Name = "txtKalan";
             this.txtKalan.Size = new System.Drawing.Size(187, 29);
             this.txtKalan.TabIndex = 5;
@@ -99,7 +103,7 @@ namespace YurtKayıtSistemi
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label3.Location = new System.Drawing.Point(59, 339);
+            this.label3.Location = new System.Drawing.Point(57, 408);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(87, 21);
             this.label3.TabIndex = 4;
@@ -108,12 +112,13 @@ namespace YurtKayıtSistemi
             // btnOdeme
             // 
             this.btnOdeme.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.btnOdeme.Location = new System.Drawing.Point(224, 376);
+            this.btnOdeme.Location = new System.Drawing.Point(224, 453);
             this.btnOdeme.Name = "btnOdeme";
             this.btnOdeme.Size = new System.Drawing.Size(95, 33);
             this.btnOdeme.TabIndex = 6;
             this.btnOdeme.Text = "Ödeme Al";
             this.btnOdeme.UseVisualStyleBackColor = true;
+            this.btnOdeme.Click += new System.EventHandler(this.btnOdeme_Click);
             // 
             // dataGridView1
             // 
@@ -129,6 +134,7 @@ namespace YurtKayıtSistemi
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(443, 185);
             this.dataGridView1.TabIndex = 7;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
             // 
             // yurtOtomasyonuDataSet2
             // 
@@ -168,17 +174,58 @@ namespace YurtKayıtSistemi
             this.ogrKalanBorcDataGridViewTextBoxColumn.HeaderText = "OgrKalanBorc";
             this.ogrKalanBorcDataGridViewTextBoxColumn.Name = "ogrKalanBorcDataGridViewTextBoxColumn";
             // 
+            // txtOgrencisoyad
+            // 
+            this.txtOgrencisoyad.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtOgrencisoyad.Location = new System.Drawing.Point(184, 313);
+            this.txtOgrencisoyad.Name = "txtOgrencisoyad";
+            this.txtOgrencisoyad.Size = new System.Drawing.Size(187, 29);
+            this.txtOgrencisoyad.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(57, 313);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(129, 21);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Ögrenci Soyadı: ";
+            // 
+            // txtOdenen
+            // 
+            this.txtOdenen.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtOdenen.Location = new System.Drawing.Point(184, 360);
+            this.txtOdenen.Name = "txtOdenen";
+            this.txtOdenen.Size = new System.Drawing.Size(187, 29);
+            this.txtOdenen.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(57, 360);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(80, 21);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Ödenen : ";
+            // 
             // FrmOdemeler
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.ClientSize = new System.Drawing.Size(447, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(447, 502);
+            this.Controls.Add(this.txtOdenen);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtOgrencisoyad);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.btnOdeme);
             this.Controls.Add(this.txtKalan);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.txtOdenen);
+            this.Controls.Add(this.txtOgrenciad);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.txtOgrid);
             this.Controls.Add(this.label1);
@@ -200,7 +247,7 @@ namespace YurtKayıtSistemi
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox txtOgrid;
-        private System.Windows.Forms.TextBox txtOdenen;
+        private System.Windows.Forms.TextBox txtOgrenciad;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox txtKalan;
         private System.Windows.Forms.Label label3;
@@ -213,5 +260,9 @@ namespace YurtKayıtSistemi
         private System.Windows.Forms.DataGridViewTextBoxColumn ogrAdDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn ogrSoyadDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn ogrKalanBorcDataGridViewTextBoxColumn;
+        private System.Windows.Forms.TextBox txtOgrencisoyad;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtOdenen;
+        private System.Windows.Forms.Label label5;
     }
 }
--- YurtKayıtSistemi/FrmOdemeler.cs
+++ YurtKayıtSistemi/FrmOdemeler.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace YurtKayıtSistemi
 {
@@ -16,12 +17,47 @@ namespace YurtKayıtSistemi
         {
             InitializeComponent();
         }
-
+        SqlBaglantim bgl = new SqlBaglantim();
         private void FrmOdemeler_Load(object sender, EventArgs e)
         {
             // TODO: This line of code loads data into the 'yurtOtomasyonuDataSet2.Borclar' table. You can move, or remove it, as needed.
             this.borclarTableAdapter.Fill(this.yurtOtomasyonuDataSet2.Borclar);
 
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int secilen;
+            string id, ad, soyad, kalan;
+            secilen = dataGridView1.SelectedCells[0].RowIndex;
+            id = dataGridView1.Rows[secilen].Cells[0].Value.ToString();
+            ad = dataGridView1.Rows[secilen].Cells[1].Value.ToString();
+            soyad = dataGridView1.Rows[secilen].Cells[2].Value.ToString();
+            kalan = dataGridView1.Rows[secilen].Cells[3].Value.ToString();
+
+            txtOgrid.Text = id;
+            txtOgrenciad.Text = ad;
+            txtOgrencisoyad.Text = soyad;
+            txtKalan.Text = kalan;
+        }
+
+        private void btnOdeme_Click(object sender, EventArgs e)
+        {
+            //odenen tutarı kalan tutardan düşme
+            int odenen, kalan, yeniborc;
+            odenen = Convert.ToInt16(txtOdenen.Text);
+            kalan = Convert.ToInt16(txtKalan.Text);
+            yeniborc = kalan - odenen;
+            txtKalan.Text = yeniborc.ToString();
+
+            // yeni tutarı veri tabanına kaydetme
+            SqlCommand komut = new SqlCommand("update Borclar set ogrkalanborc=@p1 where Ogrid=@p2", bgl.baglanti());
+            komut.Parameters.AddWithValue("@p2", txtOgrid.Text);
+            komut.Parameters.AddWithValue("@p1", txtKalan.Text);
+            komut.ExecuteNonQuery();
+            bgl.baglanti().Close();
+            MessageBox.Show("Borç Ödendi");
+            this.borclarTableAdapter.Fill(this.yurtOtomasyonuDataSet2.Borclar);
         }
     }
 }
